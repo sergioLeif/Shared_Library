c99375657a2ba9b8b756f7043fb6f642d3a9fb6d
sergioLeif <sgarcide@everis.com>
2018-03-16 12:42:29 +0100

    nodo

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -12,7 +12,7 @@ def call(body) {
 	body.delegate = config
 	body()
 	// now build, based on the configuration provided
-	node () {
+	node (config.NODO) {
 		//def String jobName = env.JOB_NAME.replace("/", "_")
 		def String UUAA = config.UUAA.toUpperCase()
 		
