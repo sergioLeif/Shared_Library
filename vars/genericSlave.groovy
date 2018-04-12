9897a76a532bbe0506f8874b97a84ac7abc256a4
sergioLeif <sgarcide@everis.com>
2018-04-12 14:09:07 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -11,7 +11,7 @@ def call(body) {
 	body.delegate = config
 	body()
 	// now build, based on the configuration provided
-	dockerNode (docker) {
+	dockerNode ('docker') {
 		sh "docker run --name jenkins_slave jenkins/jnlp-slave -url http://192.168.2.127:8080 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
 		sh "docker exec -i jenkins_slave /bin/bash"
 		sh "echo el usuario es:"
