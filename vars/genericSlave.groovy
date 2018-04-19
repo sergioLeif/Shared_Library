d68a616169d290765666b3264ac81b868b064110
sergioLeif <sgarcide@everis.com>
2018-04-19 10:49:47 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -13,7 +13,7 @@ def call(body) {
 	// now build, based on the configuration provided
 	//docker.image('jenkins/jnlp-slave').inside {
 	node{
-		sh "docker run -d --rm --name ${config.UUAA}_${env.BRANCH_NAME} jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
+		sh "docker run -d --rm --name ${config.UUAA}_env.BRANCH_NAME jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
 	}
 	node ('slave') {
 	//docker-node(image: 'jenkins/jnlp-slave'){
