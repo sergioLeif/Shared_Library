1f2cc4da9015fb1f1d3a49adda783f62fa57ad36
sergioLeif <sgarcide@everis.com>
2018-04-19 10:56:45 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -13,8 +13,9 @@ def call(body) {
 	// now build, based on the configuration provided
 	//docker.image('jenkins/jnlp-slave').inside {
 	node{
+		def dateFormat = new SimpleDateFormat("yyyyMMddHHmmss")
 		def date = new Date()
-		sh "docker run -d --rm --name ${config.UUAA}_${date} jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
+		sh "docker run -d --rm --name ${config.UUAA}_${dateFormat.format(date)} jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
 	node ('slave') {
 	//docker-node(image: 'jenkins/jnlp-slave'){
 		//sh "touch prueba"
