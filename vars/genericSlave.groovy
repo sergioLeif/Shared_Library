d2b6f28d1031a8468d01d3ba47677ad1789f155f
sergioLeif <sgarcide@everis.com>
2018-04-20 13:05:01 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -16,8 +16,8 @@ def call(body) {
 		//def dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
 		def date = new Date().format("yyyyMMddHHmmss")
 		def containerID = "${config.UUAA}_${env.BRANCH_NAME}-${date}"
-		sh "docker run -d --rm --name ${containerID} jenkins/jnlp-slave -v /var/run/docker.sock:/var/run/docker.sock -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
-		//sh "docker run -i --rm --name agent1 --init -v agent1-workdir:/home/jenkins/ xva_slave:latest java -jar /home/jenkins/slave.jar -workDir /home/jenkins/"
+		//sh "docker run -d --rm --name ${containerID} jenkins/jnlp-slave -v /var/run/docker.sock:/var/run/docker.sock -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
+		sh "docker run -i --rm --name agent1 xva_slave:latest 'java -jar /home/jenkins/slave.jar' -workDir /home/jenkins/"
 	node ('slave') {
 	//docker-node(image: 'jenkins/jnlp-slave'){
 		//sh "touch prueba"
