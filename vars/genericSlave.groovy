b9629f99ddc1f1bcd8b5990ecee147aec4345983
sergioLeif <sgarcide@everis.com>
2018-04-23 11:56:49 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -16,8 +16,8 @@ def call(body) {
 		def date = new Date().format("yyyyMMddHHmmss")
 		def containerID = "${config.UUAA}_${env.BRANCH_NAME}-${date}"
 		//sh "docker run -d --rm --name ${containerID} jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
-		sh "docker run --rm --name ${containerID} xva_slave:latest &"
-		sh "docker exec -d ${containerID} java -jar /home/jenkins/slave.jar -jnlpUrl http://192.168.2.127:8080/computer/jenkins-slave/slave-agent.jnlp -secret 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048"
+		sh "docker run --rm --name ${containerID} xva_slave/agent:latest &"
+		sh "docker exec -d ${containerID} java -jar /home/jenkins/agent.jar -jnlpUrl http://192.168.2.127:8080/computer/jenkins-slave/slave-agent.jnlp -secret 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048"
 	node ('slave') {
 		/**
 		 * DEFINICION DE VARIBLES
