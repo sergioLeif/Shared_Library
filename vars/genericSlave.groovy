9b239ae2615f0121fcf3f0eba7c078669d690d3f
sergioLeif <sgarcide@everis.com>
2018-04-24 12:12:58 +0200

    Update genericSlave.groovy

--- vars/genericSlave.groovy
+++ vars/genericSlave.groovy
@@ -18,7 +18,7 @@ def call(body) {
 		//sh "docker run -d --rm --name ${containerID} jenkins/jnlp-slave -url http://192.168.2.127:8080 -workDir=/home/jenkins/agent 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048 jenkins-slave &"
 		sh "docker run --rm --name ${containerID} xva_slave:latest &"
 		sh "docker exec -d ${containerID} /opt/java/jdk1.8.0_131/bin/java -jar /home/jenkins/agent.jar -jnlpUrl http://192.168.2.127:8080/computer/jenkins-slave/slave-agent.jnlp -secret 05d9f81216df782f70cef38ff2ae25030c18188c27a36e6cea138ab3b6b14048"
-	node ('xva') {
+	node ('slave') {
 		/**
 		 * DEFINICION DE VARIBLES
 		 */
